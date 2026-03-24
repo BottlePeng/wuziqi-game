cdda35b09ee3ea33b7ef8cfc0e7a3539ccfef3f8
BottlePeng <2329722931@qq.com>
2026-03-24 15:48:13 +0800

    部署配置存档

--- assets/script/config/networkConfig.ts
+++ assets/script/config/networkConfig.ts
@@ -1,4 +1,4 @@
 export const networkConfig = {
-    httpUrl: 'http://localhost:3000',   // 开发环境地址，发布时改为你的线上域名
-    wsUrl: 'ws://localhost:3000',       // 开发环境地址，发布时改为你的线上域名
+    httpUrl: 'http://124.222.135.170',   // 开发环境地址，发布时改为你的线上域名
+    wsUrl: 'ws://124.222.135.170',       // 开发环境地址，发布时改为你的线上域名
 }
